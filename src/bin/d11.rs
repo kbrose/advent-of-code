4402d1b441b5fc0bad843f6e989f08284d942738
Kevin Rose <kevin+gh@maypark.com>
2023-12-15 16:21:14 -0600

    day 11

--- src/bin/d11.rs
+++ src/bin/d11.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashSet, fs};
 
-fn parse_input(contents: &String) -> Vec<(usize, usize)> {
+fn parse_input(contents: &String, expansion_factor: usize) -> Vec<(usize, usize)> {
     let mut locations = vec![];
     for (i, line) in contents.split('\n').filter(|l| l.len() > 0).enumerate() {
         for j in line
@@ -29,7 +29,10 @@ fn parse_input(contents: &String) -> Vec<(usize, usize)> {
         .map(|(i, j)| {
             let unoccupied_row_count = i - occupied_rows.iter().filter(|r| r < &&i).count();
             let unoccupied_col_count = j - occupied_cols.iter().filter(|r| r < &&j).count();
-            (i + unoccupied_row_count, j + unoccupied_col_count)
+            (
+                i + unoccupied_row_count * (expansion_factor - 1),
+                j + unoccupied_col_count * (expansion_factor - 1),
+            )
         })
         .collect();
     locations
@@ -43,9 +46,9 @@ fn absolute_difference(a: usize, b: usize) -> usize {
     }
 }
 
-fn compute_1(contents: &String) -> usize {
+fn compute(contents: &String, expansion_factor: usize) -> usize {
     let mut summand = 0;
-    let locations = parse_input(contents);
+    let locations = parse_input(contents, expansion_factor);
     for (l1_index, l1) in locations.iter().enumerate() {
         for l2 in locations.iter().skip(l1_index + 1) {
             summand += absolute_difference(l1.0, l2.0) + absolute_difference(l1.1, l2.1);
@@ -58,11 +61,10 @@ fn main() {
     let contents =
         fs::read_to_string("inputs/d11.txt").expect("Should have been able to read the file");
 
-    let result = compute_1(&contents);
-    assert_eq!(9693756, result);
+    let result = compute(&contents, 2);
     println!("part 1: {result}");
 
-    // let result = compute_2(contents);
+    let result = compute(&contents, 1_000_000);
     // assert_eq!(265, result);
-    // println!("part 2: {result}");
+    println!("part 2: {result}");
 }
