dcf78477a6f0834848c7aafa31ca4c79f4db9daf
Kevin Rose <kevin+gh@maypark.com>
2024-12-08 11:11:58 -0600

    Move to subfolder

---
+++ 2023/src/bin/template.rs
@@ -0,0 +1,29 @@
+use std::fs;
+
+fn parse_input(contents: &String) -> () {
+    for _line in contents.trim().split('\n') {}
+    todo!()
+}
+
+fn compute_1(contents: &String) -> u64 {
+    let _x = parse_input(contents);
+    todo!()
+}
+
+// fn compute_2(contents: &String) -> u64 {
+//     let x = parse_input(contents);
+//     todo!()
+// }
+
+fn main() {
+    let contents =
+        fs::read_to_string("inputs/d22.txt").expect("Should have been able to read the file");
+
+    let result = compute_1(&contents);
+    // assert_eq!(670984704, result);
+    println!("part 1: {result}");
+
+    // let result = compute_2(&contents);
+    // assert_eq!(262775362119547, result);
+    // println!("part 2: {result}");
+}
