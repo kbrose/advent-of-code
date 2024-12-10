83117a2bccc6729b670ccbdbb3cbce7d6aadc196
Kevin Rose <kevin+gh@maypark.com>
2024-12-10 08:38:11 -0600

    Cleaned up

--- 2024/src/bin/d10.rs
+++ 2024/src/bin/d10.rs
@@ -17,64 +17,7 @@ struct Node {
     edges: Vec<Edge>,
 }
 
-impl Node {
-    fn is_adjacent(&self, other: &Self) -> bool {
-        self.edges
-            .iter()
-            .map(|edge| edge.dest)
-            .any(|dest| dest == other.id)
-    }
-}
-
 type Graph = HashMap<Id, Node>;
-
-// fn simplify_graph(graph: Graph) -> Graph {
-//     let mut nodes = graph.nodes;
-//     let ids: Vec<Id> = nodes.keys().copied().collect();
-//     let mut removed_ids: Vec<&Id> = Vec::new();
-//     for id in ids.iter() {
-//         if (nodes[id].edges.len() == 2) && (*id != graph.start_id) && (*id != graph.end_id) {
-//             let edge1 = nodes[id].edges[0];
-//             let edge2 = nodes[id].edges[1];
-//             let mut neighbor1 = nodes[&nodes[id].edges[0].dest].clone();
-//             let mut neighbor2 = nodes[&nodes[id].edges[1].dest].clone();
-//             macro_rules! update_edges {
-//                 ($n1:ident, $n2:ident, $e:ident) => {
-//                     if $n1.is_adjacent(&nodes[id]) {
-//                         $n1.edges = $n1
-//                             .edges
-//                             .iter()
-//                             .map(|edge| {
-//                                 if edge.dest == *id {
-//                                     Edge {
-//                                         dest: $n2.id,
-//                                         dist: edge.dist + $e.dist,
-//                                     }
-//                                 } else {
-//                                     edge.clone()
-//                                 }
-//                             })
-//                             .collect();
-//                     }
-//                 };
-//             }
-//             update_edges!(neighbor1, neighbor2, edge2);
-//             update_edges!(neighbor2, neighbor1, edge1);
-//             nodes.insert(neighbor1.id, neighbor1);
-//             nodes.insert(neighbor2.id, neighbor2);
-//             removed_ids.push(id);
-//         }
-//     }
-//     for id in removed_ids {
-//         nodes.remove(id);
-//     }
-
-//     Graph {
-//         nodes,
-//         start_id: graph.start_id,
-//         end_id: graph.end_id,
-//     }
-// }
 
 fn parse_input(contents: &str) -> Graph {
     let map: Vec<Vec<u8>> = contents
@@ -148,50 +91,6 @@ fn parse_input(contents: &str) -> Graph {
 
     HashMap::from_iter(nodes.iter().map(|n| (n.id, n.clone())))
 }
-
-// #[derive(Debug, PartialEq, Eq, Clone)]
-// struct Path {
-//     curr: Id,
-//     visited: HashSet<Id>,
-//     dist: usize,
-// }
-
-// fn find_longest_path(graph: Graph) -> usize {
-//     let mut possible_paths: Vec<Path> = vec![Path {
-//         curr: graph.start_id,
-//         visited: HashSet::new(),
-//         dist: 0,
-//     }];
-//     let mut longest_path_dist: usize = 0;
-//     while let Some(mut path) = possible_paths.pop() {
-//         if path.curr == graph.end_id {
-//             longest_path_dist = std::cmp::max(longest_path_dist, path.dist);
-//         }
-//         path.visited.insert(path.curr);
-//         let mut edges: Vec<&Edge> = graph.nodes[&path.curr]
-//             .edges
-//             .iter()
-//             .filter(|edge| !path.visited.contains(&edge.dest))
-//             .collect();
-//         // Small optimization: We'll reuse `path` on the final edge rather than cloning it.
-//         // Saves about 8% runtime on part 2.
-//         // NOTE: edges.len() - 1 may underflow, but that's fine b/c in
-//         //       that case the iterator is empty anyway
-//         for edge in edges.iter().take(edges.len() - 1) {
-//             let mut new_path = path.clone();
-//             new_path.curr = edge.dest;
-//             new_path.dist += edge.dist;
-//             possible_paths.push(new_path);
-//         }
-//         if let Some(edge) = edges.pop() {
-//             path.curr = edge.dest;
-//             path.dist += edge.dist;
-//             possible_paths.push(path);
-//         }
-//     }
-
-//     longest_path_dist
-// }
 
 fn compute_1(contents: &str) -> usize {
     let graph = parse_input(contents);
