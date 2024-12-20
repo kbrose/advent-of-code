03c01a66dff0721df49c056e29899b5d48edf91e
Kevin Rose <kevin+gh@maypark.com>
2024-12-20 08:44:34 -0600

    Faster day 18

--- 2024/src/bin/d18.rs
+++ 2024/src/bin/d18.rs
@@ -1,6 +1,5 @@
 use std::{
-    cmp::{Ordering, Reverse},
-    collections::{BinaryHeap, HashSet},
+    collections::{HashSet, VecDeque},
     fs,
 };
 
@@ -43,18 +42,6 @@ const END: Point = Point {
 #[derive(Debug, PartialEq, Eq, Hash)]
 struct Edge(Point, u64);
 
-impl PartialOrd for Edge {
-    fn partial_cmp(&self, other: &Edge) -> Option<Ordering> {
-        Some(self.1.cmp(&other.1))
-    }
-}
-
-impl Ord for Edge {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.1.cmp(&other.1)
-    }
-}
-
 fn parse_input(contents: &str) -> Vec<Point> {
     contents
         .trim()
@@ -69,31 +56,35 @@ fn parse_input(contents: &str) -> Vec<Point> {
         .collect()
 }
 
+fn checked_adjustment(new_point: Point) -> Option<Point> {
+    // Only check one side of the bounds,
+    // we used wrapping_sub to guarantee that 0 - 1 == MAX
+    if new_point.i > BOUNDS || new_point.j > BOUNDS {
+        None
+    } else {
+        Some(new_point)
+    }
+}
+
 fn find_cost_of_shortest_path(falling_ram: Vec<Point>) -> Option<u64> {
     let mut visited: HashSet<Point> = HashSet::new();
-    let mut to_visit = BinaryHeap::new();
-    to_visit.push(Reverse(Edge(START, 0)));
+    // Djikstra on an unweighted graph is just BFS. Don't need a MaxHeap
+    let mut to_visit = VecDeque::new();
+    to_visit.push_back(Edge(START, 0));
     visited.extend(falling_ram);
-    let checked_adjustment = |new_point: Point| {
-        // Only check one side of the bounds,
-        // we used wrapping_sub to guarantee that 0 - 1 == MAX
-        if new_point.i > BOUNDS || new_point.j > BOUNDS {
-            None
-        } else {
-            Some(new_point)
-        }
-    };
-    while let Some(Reverse(edge)) = to_visit.pop() {
+    while let Some(edge) = to_visit.pop_front() {
         let point = edge.0;
         let cost = edge.1;
-        if point == END {
+        if visited.contains(&point) {
+            continue;
+        } else if point == END {
             return Some(cost);
         }
-        // visited.insert(point);
+        visited.insert(point);
         for neighbor in point.neighbors().into_iter().filter_map(checked_adjustment) {
             if !visited.contains(&neighbor) {
-                visited.insert(neighbor);
-                to_visit.push(Reverse(Edge(neighbor, cost + 1)));
+                // push_back is what makes this BFS. push_front would be DFS.
+                to_visit.push_back(Edge(neighbor, cost + 1));
             }
         }
     }
@@ -105,15 +96,66 @@ fn compute_1(contents: &str) -> u64 {
     find_cost_of_shortest_path(falling_ram.into_iter().take(1024).collect()).unwrap()
 }
 
-fn compute_2(contents: &str) -> Point {
+fn compute_2(contents: &str) -> Option<Point> {
     let falling_ram = parse_input(contents);
-    for i in 1024..(falling_ram.len()) {
-        let x = find_cost_of_shortest_path(falling_ram.clone().into_iter().take(i + 1).collect());
-        if x.is_none() {
-            return falling_ram[i];
+
+    // Fill out the connected components as they exist at the end.
+    type ConnectedComponentId = u16; // u16 is enough b/c 70^2 < 2^16
+    let mut connected_component = [[ConnectedComponentId::MAX; BOUNDS + 1]; BOUNDS + 1];
+    let mut curr_component_id = 0;
+    for ram_point in falling_ram.iter() {
+        connected_component[ram_point.i][ram_point.j] = curr_component_id;
+        curr_component_id += 1;
+    }
+    for i in 0..=BOUNDS {
+        for j in 0..=BOUNDS {
+            let query_point = Point { i, j };
+            if connected_component[query_point.i][query_point.j] == ConnectedComponentId::MAX {
+                // Haven't found connected component for this point yet.
+                // Fill it out using DFS.
+                let mut visited: HashSet<Point> =
+                    HashSet::from_iter(falling_ram.clone().into_iter());
+                let mut to_visit = vec![query_point];
+                while let Some(point) = to_visit.pop() {
+                    if visited.contains(&point) {
+                        continue;
+                    }
+                    visited.insert(point);
+
+                    connected_component[point.i][point.j] = curr_component_id;
+                    for neighbor in point.neighbors().into_iter().filter_map(checked_adjustment) {
+                        if !visited.contains(&neighbor) {
+                            to_visit.push(neighbor);
+                        }
+                    }
+                }
+                curr_component_id += 1;
+            }
         }
     }
-    panic!();
+
+    // Iterate backwards over the RAM, re-connecting components that should
+    // now be connected when we remove that particular RAM block.
+    for ram_index in (0..falling_ram.len()).rev() {
+        let point = falling_ram[ram_index];
+        let point_connected_component_id = connected_component[point.i][point.j];
+        for neighbor in point.neighbors().into_iter().filter_map(checked_adjustment) {
+            if !falling_ram[..ram_index].contains(&neighbor) {
+                let neighbor_connected_component_id = connected_component[neighbor.i][neighbor.j];
+                for connected_component_i in connected_component.iter_mut() {
+                    for connected_component_ij in connected_component_i.iter_mut() {
+                        if connected_component_ij == &neighbor_connected_component_id {
+                            *connected_component_ij = point_connected_component_id;
+                        }
+                    }
+                }
+            }
+        }
+        if connected_component[START.i][START.j] == connected_component[END.i][END.j] {
+            return Some(falling_ram[ram_index]);
+        }
+    }
+    None
 }
 
 fn main() {
@@ -124,7 +166,7 @@ fn main() {
     assert_eq!(278, result);
     println!("part 1: {result}");
 
-    let result = compute_2(&contents);
+    let result = compute_2(&contents).unwrap();
     assert_eq!(Point { i: 43, j: 12 }, result);
     println!("part 2: {},{}", result.i, result.j);
 }
