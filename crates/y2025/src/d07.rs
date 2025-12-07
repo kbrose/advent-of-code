a78d09e43d62efc9ee29dcaec2e482bb78c1472e
Kevin Rose <kevin+gh@maypark.com>
2025-12-07 10:31:39 -0600

    Clean up day 7 a little

--- crates/y2025/src/d07.rs
+++ crates/y2025/src/d07.rs
@@ -4,53 +4,48 @@ use shared::Problem;
 
 #[derive(Debug, PartialEq, Eq)]
 enum Element {
-    Start,
     Splitter,
     Empty,
 }
 
-fn parse_input(contents: &str) -> Vec<Vec<Element>> {
-    contents
+fn parse_input(contents: &str) -> (Vec<Vec<Element>>, usize) {
+    let mut start_index = 0;
+    let diagram = contents
         .trim()
         .lines()
         .map(|line| {
             line.chars()
-                .map(|c| match c {
-                    'S' => Element::Start,
+                .enumerate()
+                .map(|(i, c)| match c {
+                    'S' => {
+                        start_index = i;
+                        Element::Empty
+                    }
                     '^' => Element::Splitter,
                     '.' => Element::Empty,
                     _ => panic!("Unknown character {c}"),
                 })
                 .collect()
         })
-        .collect()
+        .collect();
+    (diagram, start_index)
 }
 
 fn compute_1(contents: &str) -> u64 {
-    let diagram = parse_input(contents);
+    let (diagram, start) = parse_input(contents);
     let mut tachyon_beams: HashSet<usize> = HashSet::new();
-    for (i, element) in diagram[0].iter().enumerate() {
-        if element == &Element::Start {
-            tachyon_beams.insert(i);
-        }
-    }
-
-    let num_cols = diagram[0].len();
+    tachyon_beams.insert(start);
 
     let mut split_count = 0;
-    for diagram_row in diagram[1..].into_iter() {
+    for diagram_row in diagram.into_iter() {
         let mut new_beams = HashSet::new();
         for tachyon_beam in tachyon_beams {
             match diagram_row[tachyon_beam] {
-                Element::Start => panic!("Unexpected Start in non-first row of diagram"),
                 Element::Splitter => {
                     split_count += 1;
-                    if tachyon_beam > 0 {
-                        new_beams.insert(tachyon_beam - 1);
-                    }
-                    if tachyon_beam < num_cols {
-                        new_beams.insert(tachyon_beam + 1);
-                    }
+                    // Assumption: splitters never occur on the edges
+                    new_beams.insert(tachyon_beam - 1);
+                    new_beams.insert(tachyon_beam + 1);
                 }
                 Element::Empty => {
                     new_beams.insert(tachyon_beam);
@@ -64,34 +59,21 @@ fn compute_1(contents: &str) -> u64 {
 }
 
 fn compute_2(contents: &str) -> u64 {
-    let diagram = parse_input(contents);
+    let (diagram, start) = parse_input(contents);
     let mut tachyon_beams: HashMap<usize, u64> = HashMap::new();
-    for (i, element) in diagram[0].iter().enumerate() {
-        if element == &Element::Start {
-            tachyon_beams.insert(i, 1);
-        }
-    }
+    tachyon_beams.insert(start, 1);
 
-    let num_cols = diagram[0].len();
-
-    for diagram_row in diagram[1..].into_iter() {
+    for diagram_row in diagram.into_iter() {
         let mut new_beams = HashMap::new();
         for (tachyon_beam, count) in tachyon_beams {
             match diagram_row[tachyon_beam] {
-                Element::Start => panic!("Unexpected Start in non-first row of diagram"),
                 Element::Splitter => {
-                    if tachyon_beam > 0 {
-                        let x = new_beams.entry(tachyon_beam - 1).or_insert(0);
-                        *x += count;
-                    }
-                    if tachyon_beam < num_cols {
-                        let x = new_beams.entry(tachyon_beam + 1).or_insert(0);
-                        *x += count;
-                    }
+                    // Assumption: splitters never occur on the edges
+                    *new_beams.entry(tachyon_beam - 1).or_insert(0) += count;
+                    *new_beams.entry(tachyon_beam + 1).or_insert(0) += count;
                 }
                 Element::Empty => {
-                    let x = new_beams.entry(tachyon_beam).or_insert(0);
-                    *x += count;
+                    *new_beams.entry(tachyon_beam).or_insert(0) += count;
                 }
             }
         }
